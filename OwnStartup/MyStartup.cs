0be6c4cb1435455676fdd3ed47325e82ab8d78ea
Ryabik <ryabik.true@gmail.com>
2020-05-11 20:24:05 +0300

    ownStartup v.1.1 (added Startup)

---
+++ OwnStartup/MyStartup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OwnStartup
+{
+    public class MyStartup
+    {
+        private IConfiguration _myConfig { get; }
+        public MyStartup(IConfiguration config)
+        {
+            config = _myConfig;
+        }
+            public void ConfigureServices(IServiceCollection services)
+            {
+
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+                 app.UseRouting();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync(_myConfig["ThisTest"]);
+                    });
+                   
+                });
+            }
+        
+    }
+}
--- OwnStartup/Program.cs
+++ OwnStartup/Program.cs
@@ -20,7 +20,7 @@ namespace OwnStartup
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<MyStartup>();
                 });
     }
 }
